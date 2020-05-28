ed33c4e926c7ed2f51d91b68cb4338ff364067b6
Øyvind Kolås <pippin@gimp.org>
2020-05-28 02:47:10 +0200

    improve tests

--- ctx.c
+++ ctx.c
@@ -146,6 +146,8 @@ int main (int argc, char **argv)
 {
   const char *source_path = NULL;
   const char *dest_path = NULL;
+  //int width =  400;
+  //int height = 300;
   int width =  400;
   int height = 300;
   float cols = 40;
