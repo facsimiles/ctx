08a4db96a46d119f73f398a646826e148cf2c490
Øyvind Kolås <pippin@gimp.org>
2020-06-06 22:04:17 +0200

    incorporate events as part of ctx

--- ctx.h
+++ ctx.h
@@ -12525,6 +12525,202 @@ void ctx_parser_feed_byte (CtxParser *parser, int byte)
 }
 #endif
 
-//#include "events.h"
+#ifndef __CTX_LIST__
+#define  __CTX_LIST__
+
+#include <stdlib.h>
+
+/* The whole ctx_list implementation is in the header and will be inlined
+ * wherever it is used.
+ */
+
+
+static inline void ctx_list_prepend_full (CtxList **list, void *data,
+    void (*freefunc)(void *data, void *freefunc_data),
+    void *freefunc_data)
+{
+  CtxList *new_=calloc (sizeof (CtxList), 1);
+  new_->next = *list;
+  new_->data=data;
+  new_->freefunc=freefunc;
+  new_->freefunc_data = freefunc_data;
+  *list = new_;
+}
+
+static inline int ctx_list_length (CtxList *list)
+{
+  int length = 0;
+  CtxList *l;
+  for (l = list; l; l = l->next, length++);
+  return length;
+}
+
+static inline void ctx_list_prepend (CtxList **list, void *data)
+{
+  CtxList *new_=calloc (sizeof (CtxList), 1);
+  new_->next= *list;
+  new_->data=data;
+  *list = new_;
+}
+
+static inline void ctx_list_remove (CtxList **list, void *data)
+{
+  CtxList *iter, *prev = NULL;
+  if ((*list)->data == data)
+    {
+      if ((*list)->freefunc)
+        (*list)->freefunc ((*list)->data, (*list)->freefunc_data);
+      prev = (void*)(*list)->next;
+      free (*list);
+      *list = prev;
+      return;
+    }
+  for (iter = *list; iter; iter = iter->next)
+    if (iter->data == data)
+      {
+        if (iter->freefunc)
+          iter->freefunc (iter->data, iter->freefunc_data);
+        prev->next = iter->next;
+        free (iter);
+        break;
+      }
+    else
+      prev = iter;
+}
+
+static inline void ctx_list_free (CtxList **list)
+{
+  while (*list)
+    ctx_list_remove (list, (*list)->data);
+}
+
+static inline void
+ctx_list_reverse (CtxList **list)
+{
+  CtxList *new_ = NULL;
+  CtxList *l;
+  for (l = *list; l; l=l->next)
+    ctx_list_prepend (&new_, l->data);
+  ctx_list_free (list);
+  *list = new_;
+}
+
+static inline void *ctx_list_last (CtxList *list)
+{
+  if (list)
+    {
+      CtxList *last;
+      for (last = list; last->next; last=last->next);
+      return last->data;
+    }
+  return NULL;
+}
+
+static inline void ctx_list_append_full (CtxList **list, void *data,
+    void (*freefunc)(void *data, void *freefunc_data),
+    void *freefunc_data)
+{
+  CtxList *new_= calloc (sizeof (CtxList), 1);
+  new_->data=data;
+  new_->freefunc = freefunc;
+  new_->freefunc_data = freefunc_data;
+  if (*list)
+    {
+      CtxList *last;
+      for (last = *list; last->next; last=last->next);
+      last->next = new_;
+      return;
+    }
+  *list = new_;
+  return;
+}
+
+static inline void ctx_list_append (CtxList **list, void *data)
+{
+  ctx_list_append_full (list, data, NULL, NULL);
+}
+
+static CtxList*
+ctx_list_merge_sorted (CtxList* list1,
+                       CtxList* list2,
+    int(*compare)(const void *a, const void *b, void *userdata), void *userdata
+)
+{
+  if (list1 == NULL)
+     return(list2);
+  else if (list2==NULL)
+     return(list1);
+
+  if (compare (list1->data, list2->data, userdata) >= 0)
+  {
+    list1->next = ctx_list_merge_sorted (list1->next,list2, compare, userdata);
+    /*list1->next->prev = list1;
+      list1->prev = NULL;*/
+    return list1;
+  }
+  else
+  {
+    list2->next = ctx_list_merge_sorted (list1,list2->next, compare, userdata);
+    /*list2->next->prev = list2;
+      list2->prev = NULL;*/
+    return list2;
+  }
+}
+
+static void
+ctx_list_split_half (CtxList*  head,
+                     CtxList** list1,
+                     CtxList** list2)
+{
+  CtxList* fast;
+  CtxList* slow;
+  if (head==NULL || head->next==NULL)
+  {
+    *list1 = head;
+    *list2 = NULL;
+  }
+  else
+  {
+    slow = head;
+    fast = head->next;
+
+    while (fast != NULL)
+    {
+      fast = fast->next;
+      if (fast != NULL)
+      {
+        slow = slow->next;
+        fast = fast->next;
+      }
+    }
+
+    *list1 = head;
+    *list2 = slow->next;
+    slow->next = NULL;
+  }
+}
+
+void ctx_list_sort (CtxList **head,
+    int(*compare)(const void *a, const void *b, void *userdata),
+    void *userdata)
+{
+  CtxList* list1;
+  CtxList* list2;
+
+  /* Base case -- length 0 or 1 */
+  if ((*head == NULL) || ((*head)->next == NULL))
+  {
+    return;
+  }
+
+  ctx_list_split_half (*head, &list1, &list2);
+  ctx_list_sort (&list1, compare, userdata);
+  ctx_list_sort (&list2, compare, userdata);
+  *head = ctx_list_merge_sorted (list1, list2, compare, userdata);
+}
+
+#endif
+
+#include "events.h"
 
 #endif
