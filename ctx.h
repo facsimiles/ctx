313d3016969cfa6796ef76fb1bef38c9a996b57d
Øyvind Kolås <pippin@gimp.org>
2020-10-06 22:51:09 +0200

    ctx: add basic /dev/mice handling

--- ctx.h
+++ ctx.h
@@ -19541,6 +19541,27 @@ int ctx_scrolled (Ctx *ctx, float x, float y, CtxScrollDirection scroll_directio
 int ctx_key_press (Ctx *ctx, unsigned int keyval,
                    const char *string, uint32_t time)
 {
+  char event_type[128]="";
+  float x, y; int b;
+  sscanf (string, "%s %f %f %i", event_type, &x, &y, &b);
+  if (!strcmp (event_type, "mouse-motion") ||
+      !strcmp (event_type, "mouse-drag"))
+  {
+    ctx_pointer_motion (ctx, x, y, 1, 0);
+    return 0;
+  }
+  else if (!strcmp (event_type, "mouse-press"))
+  {
+    ctx_pointer_press (ctx, x, y, 1, 0);
+    return 0;
+  }
+  else if (!strcmp (event_type, "mouse-release"))
+  {
+    ctx_pointer_release (ctx, x, y, 1, 0);
+    return 0;
+  }
+
+
   CtxItem *item = _ctx_detect (ctx, 0, 0, CTX_KEY_DOWN);
   CtxEvent event = {0,};
 
@@ -21078,6 +21099,151 @@ static void ctx_fb_show_frame (CtxFb *fb)
 #define CTX_MAX(a,b)  (((a)>(b))?(a):(b))
 #endif
 
+
+static int mice_has_event ();
+static char *mice_get_event ();
+static void mice_destroy ();
+static int mice_get_fd (EvSource *ev_source);
+static void mice_set_coord (EvSource *ev_source, double x, double y);
+
+static EvSource ev_src_mice = {
+  NULL,
+  (void*)mice_has_event,
+  (void*)mice_get_event,
+  (void*)mice_destroy,
+  mice_get_fd,
+  mice_set_coord
+};
+
+typedef struct Mice
+{
+  int     fd;
+  double  x;
+  double  y;
+  int     prev_state;
+} Mice;
+
+Mice *_mrg_evsrc_coord = NULL;
+
+void _mmm_get_coords (Ctx *ctx, double *x, double *y)
+{
+  if (!_mrg_evsrc_coord)
+    return;
+  if (x)
+    *x = _mrg_evsrc_coord->x;
+  if (y)
+    *y = _mrg_evsrc_coord->y;
+}
+
+static Mice  mice;
+static Mice* mrg_mice_this = &mice;
+
+static int mmm_evsource_mice_init ()
+{
+  unsigned char reset[]={0xff};
+  /* need to detect which event */
+
+  mrg_mice_this->prev_state = 0;
+  mrg_mice_this->fd = open ("/dev/input/mice", O_RDONLY | O_NONBLOCK);
+  if (mrg_mice_this->fd == -1)
+  {
+    fprintf (stderr, "error opening /dev/input/mice device, maybe add to input group if such group exist, or otherwise make the rights be satisfied.\n");
+//  sleep (1);
+    return -1;
+  }
+  write (mrg_mice_this->fd, reset, 1);
+  _mrg_evsrc_coord = mrg_mice_this;
+  return 0;
+}
+
+static void mice_destroy ()
+{
+  if (mrg_mice_this->fd != -1)
+    close (mrg_mice_this->fd);
+}
+
+static int mice_has_event ()
+{
+  struct timeval tv;
+  int retval;
+
+  if (mrg_mice_this->fd == -1)
+    return 0;
+
+  fd_set rfds;
+  FD_ZERO (&rfds);
+  FD_SET(mrg_mice_this->fd, &rfds);
+  tv.tv_sec = 0; tv.tv_usec = 0;
+  retval = select (mrg_mice_this->fd+1, &rfds, NULL, NULL, &tv);
+  if (retval == 1)
+    return FD_ISSET (mrg_mice_this->fd, &rfds);
+  return 0;
+}
+
+static char *mice_get_event ()
+{
+  const char *ret = "mouse-motion";
+  double relx, rely;
+  signed char buf[3];
+  read (mrg_mice_this->fd, buf, 3);
+  relx = buf[1];
+  rely = -buf[2];
+
+  mrg_mice_this->x += relx;
+  mrg_mice_this->y += rely;
+
+  if ((mrg_mice_this->prev_state & 1) != (buf[0] & 1))
+    {
+      if (buf[0] & 1)
+        {
+          ret = "mouse-press";
+        }
+      else
+        {
+          ret = "mouse-release";
+        }
+    }
+  else if (buf[0] & 1)
+    ret = "mouse-drag";
+
+  mrg_mice_this->prev_state = buf[0];
+
+  //if (!is_active (ev_src_mice.priv))
+  //  return NULL;
+
+  {
+    char *r = malloc (64);
+    sprintf (r, "%s %.0f %.0f %i", ret, mrg_mice_this->x, mrg_mice_this->y, mrg_mice_this->fd);
+    return r;
+  }
+
+  return NULL;
+}
+
+static int mice_get_fd (EvSource *ev_source)
+{
+  return mrg_mice_this->fd;
+}
+
+static void mice_set_coord (EvSource *ev_source, double x, double y)
+{
+  mrg_mice_this->x = x;
+  mrg_mice_this->y = y;
+}
+
+EvSource *evsource_mice_new (void)
+{
+  if (mmm_evsource_mice_init () == 0)
+    {
+      mrg_mice_this->x = 0;
+      mrg_mice_this->y = 0;
+      return &ev_src_mice;
+    }
+  return NULL;
+}
+
+
+
 static int evsource_kb_has_event (void);
 static char *evsource_kb_get_event (void);
 static void evsource_kb_destroy (int sign);
@@ -21479,7 +21645,7 @@ static int event_check_pending (CtxFb *fb)
       char *event = evsource_get_event (fb->evsource[i]);
       if (event)
       {
-        //if (fb->vt_active)
+        if (fb->vt_active)
         {
           //fprintf (stderr, "ev %s\n", event);
           ctx_key_press (fb->ctx, 0, event, 0);
@@ -22404,8 +22570,10 @@ Ctx *ctx_new_fb (int width, int height)
   ctx_flush (fb->ctx);
 
   EvSource *kb = evsource_kb_new ();
+
   fb->evsource[fb->evsource_count++] = kb;
   kb->priv = fb;
+  fb->evsource[fb->evsource_count++] = evsource_mice_new ();
 
   fb->vt_active = 1;
 //return fb->ctx;
