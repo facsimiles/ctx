03c0b59d4e4f2e9b099ac6ad2c2cdf23e9c62eab
Øyvind Kolås <pippin@gimp.org>
2020-12-21 19:18:05 +0100

    further protocol shakeup

--- src/ctx.h
+++ src/ctx.h
@@ -780,36 +780,11 @@ int ctx_pointer_press     (Ctx *ctx, float x, float y, int device_no, uint32_t t
 int ctx_pointer_drop      (Ctx *ctx, float x, float y, int device_no, uint32_t time,
                            char *string);
 
-#if 0
-////////////////////
-typedef enum {
-
-  CTX_FILL_RULE        = 'r', // fillrule - u8
-  CTX_SHADOW_OFFSET_Y  = 'y',
-  CTX_BLEND_MODE       = 'B',
-  CTX_SHADOW_BLUR      = 's',
-  CTX_SHADOW_COLOR     = 'C',
-  CTX_SHADOW_OFFSET_X  = 'x',
-  CTX_TEXT_ALIGN       = 't', // align - u8, default = CTX_TEXT_ALIGN_START
-  CTX_TEXT_BASELINE    = 'b', // baseline - u8, default = CTX_TEXT_ALIGN_ALPHABETIC
-  CTX_TEXT_DIRECTION   = 'd', // direction - u8
-  CTX_MITER_LIMIT      = 'm', // km limit - float, default = 0.0
-
-  CTX_GLOBAL_ALPHA     = 'a', // ka alpha - default=1.0
-  CTX_COMPOSITING_MODE = 'c', // kc mode - u8 , default=0
-  CTX_FONT_SIZE        = 'f', // kf size - float, default=?
-  CTX_LINE_JOIN        = 'j', // kj join - u8 , default=0
-  CTX_LINE_CAP         = 'c', // kc cap - u8, default = 0
-  CTX_LINE_WIDTH       = 'w' // kw width, default = 2.0
-} CTX_GSTATE_KEYS;
-#endif
 
 typedef enum
 {
   CTX_CONT             = '\0', // - contains args from preceding entry
   CTX_NOP              = ' ', //
-  CTX_FILL_RULE        = '!', // kr rule - u8, default = CTX_FILLE_RULE_EVEN_ODD
-  CTX_SHADOW_OFFSET_Y  = '&', // ky
   CTX_DATA             = '(', // size size-in-entries - u32
   CTX_DATA_REV         = ')', // reverse traversal data marker
   CTX_SET_RGBA_U8      = '*', // r g b a - u8
@@ -817,7 +792,6 @@ typedef enum
   // set pixel might want a shorter ascii form? or keep it an embedded
   // only option?
   CTX_SET_PIXEL        = '-', // 8bit "fast-path" r g b a x y - u8 for rgba, and u16 for x,y
-  CTX_BLEND_MODE       = '/',// kB mode - u8 , default=0
   /* optimizations that reduce the number of entries used,
    * not visible outside the drawlist compression, thus
    * using entries that cannot be used directly as commands
@@ -833,11 +807,8 @@ typedef enum
   CTX_FILL_MOVE_TO              = '7', // x y
   CTX_REL_QUAD_TO_REL_QUAD_TO   = '8', // cx1 x1 cy1 y1 cx1 x2 cy1 y1 -- s8
   CTX_REL_QUAD_TO_S16           = '9', // cx1 cy1 x y                 - s16
-  // expand with: , . : 
+  // expand with: . : 
   CTX_FLUSH            = ';',
-  CTX_SHADOW_BLUR      = '<', // ks
-  CTX_SHADOW_COLOR     = '>', // kC
-  CTX_SHADOW_OFFSET_X  = '?', // kx
 
   CTX_DEFINE_GLYPH     = '@', // unichar width - u32
   CTX_ARC_TO           = 'A', // x1 y1 x2 y2 radius
@@ -911,20 +882,32 @@ typedef enum
   CTX_EDGE             = ',',
 
   /* though expressed as two chars in serialization we have
-   * dedicated byte commands for these setters - they should be folded
-   * into CTX_SET
+   * dedicated byte commands for the setters to keep the dispatch
+   * simpler. There is no need for these to be human readable thus we go >128
+   *
+   * unused/reserved: !&<=>?#:.^_=/~%\'"
+   *
    */
-  CTX_TEXT_ALIGN       = '#', // kt align - u8, default = CTX_TEXT_ALIGN_START
-  CTX_TEXT_BASELINE    = ':', // kb baseline - u8, default = CTX_TEXT_ALIGN_ALPHABETIC
-  CTX_TEXT_DIRECTION   = '.', // kd
-  CTX_MITER_LIMIT      = '^', // km limit - float, default = 0.0
+  CTX_FILL_RULE        = 128, // kr rule - u8, default = CTX_FILLE_RULE_EVEN_ODD
+  CTX_BLEND_MODE       = 129, // kB mode - u8 , default=0
 
-  CTX_GLOBAL_ALPHA     = '_', // ka alpha - default=1.0
-  CTX_COMPOSITING_MODE = '=', // kc mode - u8 , default=0
-  CTX_FONT_SIZE        = '%', // kf size - float, default=?
-  CTX_LINE_JOIN        = '\\', // kj join - u8 , default=0
-  CTX_LINE_CAP         = '\'', // kc cap - u8, default = 0
-  CTX_LINE_WIDTH       = '"', // kw width, default = 2.0
+  CTX_MITER_LIMIT      = 130, // km limit - float, default = 0.0
+
+  CTX_LINE_JOIN        = 131, // kj join - u8 , default=0
+  CTX_LINE_CAP         = 132, // kc cap - u8, default = 0
+  CTX_LINE_WIDTH       = 133, // kw width, default = 2.0
+  CTX_GLOBAL_ALPHA     = 134, // ka alpha - default=1.0
+  CTX_COMPOSITING_MODE = 135, // kc mode - u8 , default=0
+
+  CTX_FONT_SIZE        = 136, // kf size - float, default=?
+  CTX_TEXT_ALIGN       = 137, // kt align - u8, default = CTX_TEXT_ALIGN_START
+  CTX_TEXT_BASELINE    = 138, // kb baseline - u8, default = CTX_TEXT_ALIGN_ALPHABETIC
+  CTX_TEXT_DIRECTION   = 139, // kd
+
+  CTX_SHADOW_BLUR      = 140, // ks
+  CTX_SHADOW_COLOR     = 141, // kC
+  CTX_SHADOW_OFFSET_X  = 142, // kx
+  CTX_SHADOW_OFFSET_Y  = 143, // ky
   // items marked with % are currently only for the parser
   // for instance for svg compatibility or simulated/converted color spaces
   // not the serialization/internal render stream
