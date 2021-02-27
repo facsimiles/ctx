db8956fc44563315ecab187034e51008e626b31a
Øyvind Kolås <pippin@gimp.org>
2021-02-27 18:47:51 +0100

    implement file overrides for get_contents

--- src/util.h
+++ src/util.h
@@ -32,6 +32,48 @@ static int ctx_str_is_number (const char *str)
 }
 
 #if CTX_FONTS_FROM_FILE
+
+typedef struct CtxFileContent
+{
+  char *path;
+  unsigned char *contents;
+  long  length;
+  int   free_data;
+} CtxFileContent;
+
+CtxList *registered_contents = NULL;
+
+void
+ctx_file_register_contents (const char *path,
+                            const unsigned char *contents,
+                            long length,
+                            int  free_data)
+{
+  // if (path[0] != '/') && strchr(path, ':')) 
+  //   with this check regular use is faster, but we lose
+  //   generic filesystem overrides..
+  for (CtxList *l = registered_contents; l; l = l->next)
+  {
+    CtxFileContent *c = l->data;
+    if (!strcmp (c->path, path))
+    {
+       if (c->free_data)
+       {
+         free (c->contents);
+       }
+       c->free_data = free_data;
+       c->contents = (unsigned char*)contents;
+       c->length = length;
+       return;
+    }
+  }
+  CtxFileContent *c = calloc (sizeof (CtxFileContent), 1);
+  c->free_data = free_data;
+  c->contents = (unsigned char*)contents;
+  c->length    = length;
+  ctx_list_append (&registered_contents, c);
+}
+
 int
 _ctx_file_get_contents (const char     *path,
                         unsigned char **contents,
@@ -41,6 +83,18 @@ _ctx_file_get_contents (const char     *path,
   long  size;
   long  remaining;
   char *buffer;
+  for (CtxList *l = registered_contents; l; l = l->next)
+  {
+    CtxFileContent *c = l->data;
+    if (!strcmp (c->path, path))
+    {
+      contents = malloc (c->length+1);
+      contents[c->length]=0;
+      if (length) *length = c->length;
+      return 0;
+    }
+  }
+
   file = fopen (path, "rb");
   if (!file)
     { return -1; }
@@ -67,6 +121,7 @@ _ctx_file_get_contents (const char     *path,
   buffer[size] = 0;
   return 0;
 }
+
 
 void
 _ctx_file_set_contents (const char     *path,
