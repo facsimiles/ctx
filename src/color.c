9784672cc58a16b63e237a1030b6890a662e6e9f
Rahix <rahix@rahix.de>
2021-04-14 22:23:32 +0200

    color: Add ctx_rgba8_stroke()
    Similar to ctx_rgba8(), add a method for the stroke color.

--- src/color.c
+++ src/color.c
@@ -892,6 +892,11 @@ ctx_rgba8 (Ctx *ctx, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 #endif
 }
 
+void ctx_rgba8_stroke (Ctx *ctx, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+  ctx_rgba_stroke (ctx, r/255.0f, g/255.0f, b/255.0f, a/255.0f);
+}
+
 
 #endif 
 
--- src/ctx.h
+++ src/ctx.h
@@ -248,6 +248,8 @@ void ctx_named_source (Ctx *ctx, const char *name);
 
 void ctx_rgba_stroke   (Ctx *ctx, float r, float g, float b, float a);
 void ctx_rgb_stroke    (Ctx *ctx, float r, float g, float b);
+void ctx_rgba8_stroke  (Ctx *ctx, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
 void ctx_gray_stroke   (Ctx *ctx, float gray);
 void ctx_drgba_stroke  (Ctx *ctx, float r, float g, float b, float a);
 void ctx_cmyka_stroke  (Ctx *ctx, float c, float m, float y, float k, float a);
