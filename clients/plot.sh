1c59ac703b7eea5cff87b034f9267d8f9926379b
Øyvind Kolås <pippin@gimp.org>
2021-04-30 23:58:59 +0200

    tests: add more coordinates to plot.sh's polyline

--- clients/plot.sh
+++ clients/plot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# a plot using terminal cell relative coordinates
+# Make plot using terminal cell relative coordinates
 
 echo -ne "\e[?200h
 rgba 1 1 1 0.9
@@ -20,9 +20,20 @@ translate 1@ 5@
 scale 1@ 8@
 moveTo
 0.0 0.0
-lineTo 1.0 0.5
-       3.0 -0.5
-       4.0 0.5
+lineTo 1 0.5
+       3 -0.5
+       4 0.5
+       5 -0.5
+       6 0.5
+       7 -0.5
+       8 0.5
+       9 -0.5
+       10 0.25
+       11 -0.25
+       12  0.25
+       13 -0.125
+       14  0.125
+       16  0.0
 restore
 lineWidth 0.1@
 rgbS 1 0 0
