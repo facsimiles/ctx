9534a0130a2837ca7ea0c2991ba26e1758660241
Øyvind Kolås <pippin@gimp.org>
2022-09-12 01:59:49 +0200

    build: make fs creator handle binary files

--- tools/gen_fs.sh
+++ tools/gen_fs.sh
@@ -2,21 +2,21 @@
 
 echo "typedef struct _CacheEntry2{const char *uri;"
 echo "long length; "
-echo "const char *data;}CacheEntry2;"
-echo "static CacheEntry2 itk_fs[]={"
+echo "const unsigned char *data;}CacheEntry2;"
 echo "#define ITK_HAVE_FS"
 
-remove_base=$1/
+cd $1
 
-for a in `find $1 -type f`;do
-   b=`echo $a|sed s:$remove_base::`;
+for a in `find . -type f`;do
+   xxd --include $a
+done
+
+echo "static CacheEntry2 itk_fs[]={"
+for a in `find . -type f`;do
+   b=`echo $a|sed s:\./::`;
+   c=__`echo $b|sed s:[./]:_:g`;
    echo "{\"$b\", `wc -c $a|cut -f 1 -d ' '`,";
-   cat $a | sed 's/\\/\\\\/g' |\
-            sed 's/\r/a/'     |\
-            sed 's/"/\\"/g'   |\
-            sed 's/^/"/'      |\
-            sed 's/$/\\n"/'
-            echo "},"
+   echo "$c},"
 done
 echo "{\"\", 6, \"fnord\"}," ;
 echo "{NULL,0,NULL}};";
