3d2fe47f9c2c46322e2b6cb8b90314621e240e20
Øyvind Kolås <pippin@gimp.org>
2022-09-23 13:49:00 +0200

    squoze: use size_t on hash when casting to pointer

--- squoze/squoze.h
+++ squoze/squoze.h
@@ -1,3 +1,18 @@
+/* Copyright (c) 2021-2022 Øyvind Kolås <pippin@gimp.org>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 #ifndef SQUOZE_H
 #define SQUOZE_H
 
@@ -47,7 +62,6 @@
 #endif
 
 #ifndef SQUOZE_INITIAL_POOL_SIZE
-//#define SQUOZE_INITIAL_POOL_SIZE   (1<<24)
 #define SQUOZE_INITIAL_POOL_SIZE   (1<<8)
 #endif
 
@@ -654,7 +668,7 @@ Squoze *squoze_pool_add (SquozePool *pool, const char *str)
     return interned;
   }
   else
-    return (Squoze*)(hash);
+    return (Squoze*)((size_t)hash);
 }
 
 static Squoze *squoze_lookup_struct_by_id (SquozePool *pool, squoze_id_t id);
