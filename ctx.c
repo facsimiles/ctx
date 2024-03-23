48d7e683a8ef0a480e8e08dca85229314eaaaeee
Øyvind Kolås <pippin@gimp.org>
2024-03-23 22:09:08 +0100

    ctx: default to RASTERIZER_AA == AA256

--- ctx.c
+++ ctx.c
@@ -30,6 +30,8 @@
 #define CTX_MIN_EDGE_LIST_SIZE   1024*16     // default: 1024*4
 //#define CTX_PARSER_MAXLEN      1024*1024*64 // default: 1024*1024*16
 
+#define CTX_RASTERIZER_AA        15
+
 #define CTX_HASH_COLS            6 // default: 6
 #define CTX_HASH_ROWS            5 // default: 5
 #define CTX_MAX_KEYDB            128  // default: 64
